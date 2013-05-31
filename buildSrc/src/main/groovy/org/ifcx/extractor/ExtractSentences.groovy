99069914dd09cb190c19d210d8518ea12e820413
Jim White <james.paul.white@gmail.com>
2013-05-30 17:11:24 -0700

    Extract comment sentence to one-per-line file for parser.

---
+++ buildSrc/src/main/groovy/org/ifcx/extractor/ExtractSentences.groovy
@@ -0,0 +1,33 @@
+package org.ifcx.extractor
+
+import org.gradle.api.tasks.*
+import org.ifcx.extractor.util.Sexp
+
+class ExtractSentences extends SourceTask {
+    @OutputFile
+    File extractionIndex
+
+    @OutputFile
+    File extractedSentences
+
+    @TaskAction
+    public void process()
+    {
+        extractionIndex.withPrintWriter { indexPrinter ->
+        extractedSentences.withPrintWriter { commentPrinter ->
+            source.each { input ->
+                def xml = new XmlSlurper().parse(input)
+                def extracts = xml.body.'**'.findAll { it.@'class' == 'method-extract' }
+                extracts.each {
+                    def tree = Sexp.read_one_sexp(new StringReader(it.text()))
+                    def map = Sexp.tree_to_map(tree)
+
+                    indexPrinter.println "${map.Id}\t${input.path}"
+                    commentPrinter.println map.Sentence
+                }
+            }
+        }
+        }
+    }
+
+}
--- scripts/SlurpXHTML.groovy
+++ scripts/SlurpXHTML.groovy
@@ -1,5 +1,7 @@
+import org.ifcx.extractor.util.Sexp
 
-foo = new XmlSlurper().parse(new File('../data/binary/method|com.sun.tools.javadoc.SeeTagImpl|referencedMemberName|java.lang.String|0.html'))
+//foo = new XmlSlurper().parse(new File('../data/methods/method|com.sun.tools.javac.jvm.ClassReader|isAsciiDigit|boolean|1|char.html'))
+foo = new XmlSlurper().parse(new File('../data/methods/method|com.sun.tools.doclets.formats.html.markup.HtmlWriter|getBold|java.lang.String|0.html'))
 
 //println foo
 
@@ -19,3 +21,18 @@ methods.each { method ->
 }
 
 println foo.children().size()
+
+def extracts = foo.body.'**'.findAll { it.@'class' == 'method-extract' }
+
+println "${extracts.size()} extracts"
+
+extracts.each { extract ->
+    def extract_text = extract.text()
+
+    println extract_text
+    def tree = Sexp.read_one_sexp(new StringReader(extract_text))
+    println tree
+    def m = Sexp.tree_to_map(tree)
+    println m.Comment
+    println m.Sentence
+}
